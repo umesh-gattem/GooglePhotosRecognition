57564796505ea8b662d278abdc37a9c738ee85ff
ugattem <ugattem@iu.edu>
2023-03-14 21:01:18 -0400

    Updated code

---
+++ Inception/camera_input_resnet.py
@@ -0,0 +1,52 @@
+def inception(image_name):
+    from tensorflow.keras.applications.resnet50 import ResNet50
+    from tensorflow.keras.preprocessing import image
+    from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+    import numpy as np
+
+    model = ResNet50(weights='imagenet')
+
+    img_path = image_name
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+
+    preds = model.predict(x)
+    # decode the results into a list of tuples (class, description, probability)
+    # (one such list for each sample in the batch)
+    print('Predicted:', decode_predictions(preds, top=3)[0])
+    return decode_predictions(preds, top=5)[0]
+
+import cv2
+
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("test")
+
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    cv2.imshow("test", frame)
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        print(inception(img_name))
+        img_counter += 1
+
+cam.release()
+
+cv2.destroyAllWindows()
+
